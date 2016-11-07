a603be17e4e68def03d3f99bdcc692a8148197fd
Mahen Samaranayake <wxmahen@gmail.com>
2016-11-07 19:39:30 +0530

    third commit

--- src/com/educluster/mahen/ctrl/Cartoonifier.java
+++ src/com/educluster/mahen/ctrl/Cartoonifier.java
@@ -1,4 +1,3 @@
-
 package com.educluster.mahen.ctrl;
 
 import java.awt.Color;
@@ -7,24 +6,41 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JProgressBar;
 
 /**
  *
  * @author Mahen Samaranayake
  */
 public class Cartoonifier {
-
-    public static void main(String[] args) {
-        File folder = new File("a");
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            System.out.println(file.getName());
-            convert(file.getName());
-        }
+    
+    String input_folder = "input";
+    String output_folder = "output";
+    
+    public void cartoonifyAll(JProgressBar progressBar, JButton button) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                button.setEnabled(false);
+                File folder = new File(input_folder);
+                File[] files = folder.listFiles();
+                int number_of_files = files.length;
+                int progress = 0;
+                for (File file : files) {
+                    progress++;
+                    progressBar.setValue((100 * progress) / number_of_files);
+                    cartoonify(file.getName());
+                }
+                progressBar.setValue(0);
+                button.setEnabled(true);
+            }
+        }).start();
     }
     
-    public static void convert(String url){try {
-            BufferedImage bi = ImageIO.read(new File("a\\"+url));
+    public void cartoonify(String url) {
+        try {
+            BufferedImage bi = ImageIO.read(new File(input_folder + "\\" + url));
             int x = bi.getWidth();
             int y = bi.getHeight();
             List<Integer> lines_r = new ArrayList<>();
@@ -46,7 +62,6 @@ public class Cartoonifier {
                 lines_r.add(val_r / y);
                 lines_g.add(val_g / y);
                 lines_b.add(val_b / y);
-                System.out.println(1 + " - " + i);
             }
             for (int i = 0; i < x; i++) {
                 for (int j = 0; j < y; j++) {
@@ -56,18 +71,18 @@ public class Cartoonifier {
                     int blue = col.getBlue();
                     Color color = Color.white;
                     int var = 30;
-                    if (red < lines_r.get(i)-var || green < lines_g.get(i)-var || blue < lines_b.get(i)-var) {
+                    if (red < lines_r.get(i) - var || green < lines_g.get(i) - var || blue < lines_b.get(i) - var) {
                         color = Color.BLACK;
                     }
                     try {
-                        bi.setRGB(i,j, color.getRGB());
+                        bi.setRGB(i, j, color.getRGB());
                     } catch (Exception e) {
                     }
                 }
-                System.out.println(2 + " - " + i);
             }
-            ImageIO.write(bi, "jpg", new File("a\\"+"converted "+url));
+            ImageIO.write(bi, "jpg", new File(output_folder + "\\" + url));
         } catch (Exception e) {
             e.printStackTrace();
-        }}
+        }
+    }
 }
